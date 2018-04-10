79f94d0c7d757886ba56c29f49086745463c4fbd
SammarTahir <G00347526@gmit.ie>
2018-04-10 16:08:45 +0100

    Calculator function added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CalculatorPage } from '../pages/calculator/calculator';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CalculatorPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CalculatorPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CalculatorPage } from '../calculator/calculator';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  openCalculator(){
+    this.navCtrl.push(CalculatorPage);
+  }
 }
